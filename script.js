021b2b80c8bc2038e138e519e73860f6aa7c30b8
Aman <amanyadav923949@gmail.com>
2024-02-29 23:49:14 +0530

    Added Website
    Added images and layout of the website
    
    Co-Authored-By: Aditya7005446231 <142924626+Aditya7005446231@users.noreply.github.com>

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ window.onscroll = () =>{
     navbar.classList.remove('active');
 }
 
+var swiper = new Swiper(".gallery-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    spaceBetween:20,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1,
+        },
+        700:{
+            slidesPerView:2,
+        },
+    }
+})
